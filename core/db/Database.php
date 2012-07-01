836e28865d02934c752ebe63381e2682ef00853e
Jackson Owens <jackson_owens@brown.edu>
2012-07-01 15:09:08 -0400

    Turned Database class into an actual PDO wrapper

--- core/db/Database.php
+++ core/db/Database.php
@@ -3,16 +3,15 @@
 namespace \esprit\core\db;
 
 /**
- * 
  * A database wrapper for PDO. Allows for more functionality to be built onto PDO
  * 
  * @author jbowens
- *
  */
-class Database extends PDO {
+class Database {
+
+    protected $dbh;
 
 	/**
-	 * 
 	 * Default constructor for a datbase. Constructs from a DSN.
 	 * 
 	 * @param string $dsn
@@ -22,13 +21,66 @@ class Database extends PDO {
 	 */
 	public function __construct($dsn, $username, $password, $driver_options = array()) {
 	
-		$result = parent::__construct($dsn, $username, $password, $driver_options);
+		$this->dbh = new PDO($dsn, $username, $password, $driver_options);
 		
-		$this->setAttribute(self::ATTR_STATEMENT_CLASS, array('DBStatement', array($this)));
-		
-		return $result;
-	
-	}
-	
-}
+		$this->dbh->setAttribute(self::ATTR_STATEMENT_CLASS, array('Statement', array($this)));
 
+	}
+
+    public function beginTransaction() {
+        return $this->dbh->beginTransaction();
+    }
+
+    public function commit() {
+        return $this->dbh->commit();
+    }
+
+    public function errorCode() {
+        return $this->dbh->errorCode();
+    }
+
+    public function errorInfo() {
+        return $this->dbh->errorInfo();
+    }
+
+    public function exec($statement) {
+        return $this->dbh->exec($statement);
+    }
+
+    public function getAttribute( $attribute ) {
+        return $this->dbh->getAttribute( $attribute );
+    }
+
+    public static function getAvailableDrivers() {
+        return PDO::getAvailableDrivers();
+    }
+
+    public function inTransaction() {
+        return $this->dbh->inTransaction();
+    }
+
+    public function lastInsertId($name = null) {
+        return $this->dbh->lastInsertId();
+    }
+
+    public function prepare($statement, $driver_options = array()) {
+        return $this->dbh->prepare($statement, $driver_options);
+    }
+
+    public function query($statement) {
+        return $this->dbh->query($statement);
+    }
+	
+    public function quote($string, $parameter_type = PDO::PARAM_STR) {
+        return $this->dbh->quote($string, $parameter_type);
+    }
+
+    public function rollBack() {
+        return $this->dbh->rollBack();
+    }
+
+    public function setAttribute($attribute, $value) {
+        return $this->dbh->setAttribute($attribute, $value);
+    }
+
+}
