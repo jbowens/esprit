ee660d8560d976bb83a21e1c6f9927fed1041a78
Jackson Owens <jackson_owens@brown.edu>
2012-07-08 20:52:42 -0400

    Moved the autoloader to autoloader.php

--- autoloader.php
+++ autoloader.php
@@ -7,5 +7,27 @@ require_once "core/Controller.php";
 /**
  * This file adds an autoloader for all default esprit classes.
  */
-spl_autoload_register(__NAMESPACE__.'\core\Controller::autoload', true);
+
+function autoload( $class ) {
+    $classPieces = explode("\\", $class);
+
+    if( count($classPieces) == 0 )
+        return false;
+
+    if( $classPieces[0] != 'esprit' )
+        return false;
+
+    unset($classPieces[0]);
+
+    $file = implode('/', $classPieces).'.php';
+
+    if( @file_exists( __DIR__ . DIRECTORY_SEPARATOR . $file ) )
+    {
+        require_once( __DIR__ . DIRECTORY_SEPARATOR . $file );
+        return true;
+    }
+}
+
+
+spl_autoload_register(__NAMESPACE__.'\autoload', true);
 
--- core/Controller.php
+++ core/Controller.php
@@ -213,28 +213,5 @@ class Controller {
         $this->logger->log( LogEventFactory::createFromException($ex, $sourceDesc) ); 
     }
 
-    /**
-     * Defines an autoloader for esprit classes.
-     */
-    public static function autoload( $class ) {
-        $classPieces = explode("\\", $class);
-
-        if( count($classPieces) == 0 )
-            return false;
-
-        if( $classPieces[0] != 'esprit' )
-            return false;
-
-        unset($classPieces[0]);
-
-        $file = implode('/', $classPieces).'.php';
-
-        if( @file_exists( __DIR__ . DIRECTORY_SEPARATOR . $file ) )
-        {
-            require_once( __DIR__ . DIRECTORY_SEPARATOR . $file );
-            return true;
-        }
-    }
-
 }
 
