936582ec4743152defe9e496d51ba384e2c05896
Jackson Owens <jackson_owens@brown.edu>
2012-07-30 22:59:41 -0400

    Added Time class

---
+++ core/Time.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace esprit\core;
+
+/**
+ * A class with time related functions and constants.
+ *
+ * @author jbowens
+ */
+class Time {
+
+    const AN_HOUR = 3600;
+    const A_DAY = 86400;
+    const A_WEEK = 604800;
+    const A_MONTH = 2592000;
+    const A_YEAR = 31536000;
+
+}
