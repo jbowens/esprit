4014144b4d307f15903320bc696b4db71185c346
Jackson Owens <jackson_owens@brown.edu>
2012-08-27 17:13:15 -0400

    Added action database tables

--- scripts/setup/create-database-mysql.sql
+++ scripts/setup/create-database-mysql.sql
@@ -29,3 +29,35 @@ COLLATE = utf8_bin;
 
 -- Create an index on the translation identifier and language id
 ALTER TABLE translations ADD INDEX translation_lookup (languageid, translationIdentifier) USING BTREE;
+
+-- Create the actions table
+CREATE TABLE IF NOT EXISTS `actions` (
+    actionid                BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    typeid                  MEDIUMINT UNSIGNED NOT NULL,
+    dateRecorded            INT UNSIGNED NOT NULL,
+    sessionid               CHAR(32) NOT NULL,
+    PRIMARY KEY(actionid)
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
+
+-- Create an index on the typeid
+ALTER TABLE actions ADD INDEX actions_type (typeid) USING BTREE;
+
+-- Create an idnex on the session
+ALTER TABLE actions ADD INDEX actions_session (sessionid) USING BTREE;
+
+-- Create the action types table
+CREATE TABLE IF NOT EXISTS `action_types` (
+    typeid                  MEDIUMINT UNSIGNED NOT NULL,
+    identifier              VARCHAR(60) NOT NULL,
+    PRIMARY KEY(typeid)
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
+
+-- Create a unique index on the action identifier
+CREATE UNIQUE INDEX action_identifier ON action_types (identifier) USING BTREE;
+
