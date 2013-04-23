66b06017e736eb95d81d1f98e2ec0b240a4c937f
Jackson Owens <jackson_owens@brown.edu>
2013-04-23 14:54:49 -0400

    retabbed

--- core/db/DatabaseManager.php
+++ core/db/DatabaseManager.php
@@ -15,46 +15,46 @@ use \PDOException as PDOException;
  */
 class DatabaseManager {
 
-	/* A map of handles to database connections */
-	protected $databaseConnections = array();
+    /* A map of handles to database connections */
+    protected $databaseConnections = array();
 
     /* A map of lazy database connections. These connections have not been made yet,
      * but will be created if needed. */
     protected $lazyDatabaseConnections = array();
 
-	/* The default username */
-	protected $defaultUsername;
-	
-	/* The default password */
-	protected $defaultPass;
+    /* The default username */
+    protected $defaultUsername;
+    
+    /* The default password */
+    protected $defaultPass;
 
     protected $logger;
-	
-	/**
-	 * Creates a new database manager. This requres a default DSN and corresponding
-	 * default user and password. 
-	 * 
-	 * @param string $defaultDsn  the dsn of the default database
-	 * @param string $defaultUser  the default database username
-	 * @param string $defaultPass  the default database password
+    
+    /**
+     * Creates a new database manager. This requres a default DSN and corresponding
+     * default user and password. 
+     * 
+     * @param string $defaultDsn  the dsn of the default database
+     * @param string $defaultUser  the default database username
+     * @param string $defaultPass  the default database password
      * @param Logger $logger  the logger to use for db operations
-	 */
-	public function __construct($defaultDsn, $defaultUser, $defaultPass, $logger) {
-		$this->defaultUser = $defaultUser;
-		$this->defaultPass = $defaultPass;
+     */
+    public function __construct($defaultDsn, $defaultUser, $defaultPass, $logger) {
+        $this->defaultUser = $defaultUser;
+        $this->defaultPass = $defaultPass;
         $this->logger = $logger;
-		$this->lazyConnectToDatabase("default", $defaultDsn, $defaultUser, $defaultPass) ;
-	}
-	
-	/**
-	 * Returns the datbase connection associated with the given handle.
-	 * 
-	 * @param string $handle  the handle of the database connection
-	 * @throws exceptions\NonexistentDatabaseException if no such handle exists
-	 * 
-	 * @return  a database connection (a PDO object)
-	 */
-	public function getDb($handle = "default") {
+        $this->lazyConnectToDatabase("default", $defaultDsn, $defaultUser, $defaultPass) ;
+    }
+    
+    /**
+     * Returns the datbase connection associated with the given handle.
+     * 
+     * @param string $handle  the handle of the database connection
+     * @throws exceptions\NonexistentDatabaseException if no such handle exists
+     * 
+     * @return  a database connection (a PDO object)
+     */
+    public function getDb($handle = "default") {
         if( ! isset( $this->databaseConnections[$handle] ) )
         {
             // Is this an uninitialized lazy connection?
@@ -69,37 +69,37 @@ class DatabaseManager {
                 return $this->databaseConnections[$handle];
             } else
             {
-			    throw new \esprit\core\exceptions\NonexistentDatabaseException("The database '".$handle."' does not exist.");
+                throw new \esprit\core\exceptions\NonexistentDatabaseException("The database '".$handle."' does not exist.");
             }
         }
         return $this->databaseConnections[$handle];
-	}
-	
-	/**
-	 * Connects to another database, given a handle to refer to the connection by,
-	 * a dsn, and optionally a username and password pair. If the username and password
-	 * are omitted, the default username and password provided on construction will be used.
-	 * 
-	 * @param string $handle  the handle to refer to the connection in the future
-	 * @param string $dsn  the dsn of the database
-	 * @param string $user  (optional) the database username
-	 * @param string $pass  (optional) the datbaase password
+    }
+    
+    /**
+     * Connects to another database, given a handle to refer to the connection by,
+     * a dsn, and optionally a username and password pair. If the username and password
+     * are omitted, the default username and password provided on construction will be used.
+     * 
+     * @param string $handle  the handle to refer to the connection in the future
+     * @param string $dsn  the dsn of the database
+     * @param string $user  (optional) the database username
+     * @param string $pass  (optional) the datbaase password
      *
      * @throws DatabaseConnectionException  if the connection attempt fails
-	 */
-	public function connectToDatabase($handle, $dsn, $user = null, $pass = null) {
-		
-		if( $user == null )
-			$user = $this->defaultUsername;
-		
-		if( $pass == null )
-			$pass = $this->defaultPass;
-		
-        try {	
-		    $this->databaseConnections[$handle] = new Database($dsn, $user, $pass, $this->logger, array());
+     */
+    public function connectToDatabase($handle, $dsn, $user = null, $pass = null) {
+        
+        if( $user == null )
+            $user = $this->defaultUsername;
+        
+        if( $pass == null )
+            $pass = $this->defaultPass;
+        
+        try {   
+            $this->databaseConnections[$handle] = new Database($dsn, $user, $pass, $this->logger, array());
 
             $this->logger->info("Connected to " . $dsn . " as " .$user . " under handle " . $handle, "DATABASE");
-		
+        
         } catch(PDOException $ex) {
             $this->logger->severe("Error connecting to " . $dsn, "DATABASE", array( 'handle' => $handle,
                                                                                     'dsn'    => $dsn,
