80b3f45ec0f9008b37209e9d3a5b53f3be99b9df
Jackson Owens <jackson_owens@brown.edu>
2013-05-12 13:27:46 -0400

    retabbed

--- core/Config.php
+++ core/Config.php
@@ -14,34 +14,34 @@ use \esprit\core\exceptions\BadConfigFileException as BadConfigFileException;
  */
 class Config {
 
-	/* The map of configuration options and their values */
-	protected $options;
-	
-	/**
-	 * Creates a new configuration object from the given XML file.
-	 * 
-	 * @param string $configFile  the filename of the file to load
-	 * 
-	 * @return Config a config object
-	 */
-	public static function createFromXMLFile($configFile) {
-		
-		$config = new Config();
-		
-		$xml = simplexml_load_file( $configFile );
-	
-		foreach($xml->children() as $option) {
-			$config->set($option->getName(), (String) $option);
-		}
+    /* The map of configuration options and their values */
+    protected $options;
+    
+    /**
+     * Creates a new configuration object from the given XML file.
+     * 
+     * @param string $configFile  the filename of the file to load
+     * 
+     * @return Config a config object
+     */
+    public static function createFromXMLFile($configFile) {
+        
+        $config = new Config();
+        
+        $xml = simplexml_load_file( $configFile );
+    
+        foreach($xml->children() as $option) {
+            $config->set($option->getName(), (String) $option);
+        }
 
-		return $config;
-		
-	}
-	
-	/**
-	 * Creates a new config instance.
-	 */
-	protected function __construct() {
+        return $config;
+        
+    }
+    
+    /**
+     * Creates a new config instance.
+     */
+    protected function __construct() {
         $this->options = array();
 
         // Populate the magic values
@@ -56,42 +56,42 @@ class Config {
         $this->set('esprit_commands', __DIR__ . DIRECTORY_SEPARATOR . 'commands');
         $this->set('esprit_views', __DIR__ . DIRECTORY_SEPARATOR . 'views' );
         $this->set('esprit_data', $root . DIRECTORY_SEPARATOR . 'data');
-	}
-	
-	/**
-	 * Sets a configuration option.
-	 * 
-	 * @param String $key  the key for the property
-	 * @param $val         the value of the property
-	 */
-	public function set($key, $val) {
-		$this->options[$key] = $val;
-	}
-	
-	/**
-	 * 
-	 * Retrieves the value of the config option with the given key.
-	 * 
-	 * @param string $key  the key to lookup
-	 * 
-	 * @return the key's value, or null if the key isn't set
-	 */
-	public function get($key) {
-		if( ! $this->settingExists($key) )
-			return null;
-		else
-			return $this->options[$key];
-	}
-	
-	/**
-	 * Determines if a given setting exists.
-	 * 
-	 * @param String $key  the setting to check
-	 * 
-	 * @return true iff the setting is set
-	 */
-	public function settingExists($key) {
-		return isset($this->options[$key]);
+    }
+    
+    /**
+     * Sets a configuration option.
+     * 
+     * @param String $key  the key for the property
+     * @param $val         the value of the property
+     */
+    public function set($key, $val) {
+        $this->options[$key] = $val;
+    }
+    
+    /**
+     * 
+     * Retrieves the value of the config option with the given key.
+     * 
+     * @param string $key  the key to lookup
+     * 
+     * @return the key's value, or null if the key isn't set
+     */
+    public function get($key) {
+        if( ! $this->settingExists($key) )
+            return null;
+        else
+            return $this->options[$key];
+    }
+    
+    /**
+     * Determines if a given setting exists.
+     * 
+     * @param String $key  the setting to check
+     * 
+     * @return true iff the setting is set
+     */
+    public function settingExists($key) {
+        return isset($this->options[$key]);
     }
 
     /**
@@ -117,5 +117,5 @@ class Config {
 
         return $config;
     }
-	
+    
 }
